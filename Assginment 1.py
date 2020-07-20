d10f830b3a5d479bb186b382033f26a3ca21a54e
SARVESH T MISHRA <68542357+sampandit@users.noreply.github.com>
2020-07-20 17:04:52 +0530

    Create Assginment 1.py

---
+++ Assginment 1.py
@@ -0,0 +1,10 @@
+what is python ? what are the ways to install python in your PC ?
+python was discoverd by the Guido van Rossum in 1991 but it got famous in year 2006 because of google before it java was trending much more which was discoverd  in 1995  by sun microsystem 
+click on  Python Download click the Windows link python 3.7 version
+Move this file to a more permanent location, so that you can install Python
+
+what are code environment ? types?
+ Environments are independent: you can install different packages or different versions of packages in different environments without interaction between them.
+ text editors
+ integrated development environment (IDE)
+ notebook environment
